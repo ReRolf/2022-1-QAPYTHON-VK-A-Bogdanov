57b1abec85ca942948bb95b88240fbe82d13b4d2
rerolf <bingodingo365@gmail.com>
2022-04-21 03:00:21 +0300

    Убрал постоянный вызов PageObject в тестах

--- homework2/test.py
+++ homework2/test.py
@@ -2,12 +2,7 @@ import os
 import allure
 import pytest
 from base import BaseCase
-from ui.pages.login_page import LoginPage
-from ui.pages.campaign_page import CampaignPage
-from selenium.webdriver.common.by import By
-from ui.pages.audience_page import AudiencePage
 from ui.locators import basic_locators
-
 
 
 @allure.description(
@@ -15,14 +10,12 @@ from ui.locators import basic_locators
 )
 class TestNegative(BaseCase):
     def test_email(self):
-        login = LoginPage(self.driver)
-        login.login('qwerty', 'password')
-        assert login.find(basic_locators.FailureTests.EMAIL_LOGIN_FAILURE_LOCATOR)
+        self.login_page.login('qwerty', 'password')
+        assert self.login_page.find(basic_locators.FailureTests.EMAIL_LOGIN_FAILURE_LOCATOR)
 
     def test_password(self):
-        login = LoginPage(self.driver)
-        login.login('valid_email@mail.ru', 'password')
-        assert login.find(basic_locators.FailureTests.PASSWORD_LOGIN_FAILURE_LOCATOR)
+        self.login_page.login('valid_email@mail.ru', 'password')
+        assert self.login_page.find(basic_locators.FailureTests.PASSWORD_LOGIN_FAILURE_LOCATOR)
 
 
 @allure.description(
@@ -34,9 +27,7 @@ class TestCampaign(BaseCase):
         return os.path.join(repo_root, 'user_data', 'image.jpg')
 
     def test_campaign_create(self, credentials, file_path):
-        login = LoginPage(self.driver)
-        login.login(*credentials)
-        campaign = CampaignPage(self.driver)
+        self.login_page.login(*credentials)
         url = "https://taucetistation.org/"
         title = 'Space Station 13'
         age = '16-45'
@@ -45,9 +36,10 @@ class TestCampaign(BaseCase):
         date_to = '22.04.2022'
         daily_budget = '100'
         total_budget = '200'
-        campaign.campaign_create(url, title, age, countries, date_from, date_to, daily_budget, total_budget, file_path)
-        campaign.campaign_create(url, title, age, countries, date_from, date_to, daily_budget, total_budget, file_path)
-        campaign.check_campaign_detailed(url, title, age, countries, date_from, date_to, daily_budget, total_budget)
+        self.campaign_page.campaign_create(url, title, age, countries, date_from, date_to, daily_budget, total_budget,
+                                           file_path)
+        self.campaign_page.check_campaign_detailed(url, title, age, countries, date_from, date_to, daily_budget,
+                                                   total_budget)
 
 
 @allure.description(
@@ -55,10 +47,7 @@ class TestCampaign(BaseCase):
 )
 class TestSegment(BaseCase):
     def test_segment_create(self, credentials):
-        login = LoginPage(self.driver)
-        login.login(*credentials)
-        segment = AudiencePage(self.driver)
-        segment.segment_create()
-        assert segment.find(segment.locators.SEGMENT_CREATED_LOCATOR)
-        segment = AudiencePage(self.driver)
-        segment.segment_delete()
+        self.login_page.login(*credentials)
+        self.audience_page.segment_create()
+        assert self.audience_page.find(self.audience_page.locators.SEGMENT_CREATED_LOCATOR)
+        self.audience_page.segment_delete()
